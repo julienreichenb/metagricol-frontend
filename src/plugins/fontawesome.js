68f5caa6d034085fa30d846129e22c029eb1380e
Julien Reichenbach <julien.reichenbach@hevs.ch>
2022-01-24 15:13:32 +0100

    remove fontawesome pro

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -5,17 +5,11 @@ import {
   FontAwesomeLayers,
   FontAwesomeLayersText,
 } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
 
 library.add(fas)
 library.add(fab)
-library.add(far)
-library.add(fad)
-library.add(fal)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
